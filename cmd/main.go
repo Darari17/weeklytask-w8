06bb9b35a4c85604c27704aba3bdab1ec7318cbc
farid <faridramadhan59@gmail.com>
2025-08-23 14:27:29 +0700

    feat: Invoke the display function for each task.

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/Darari17/weeklytask-w8/internals/task1"
+	"github.com/Darari17/weeklytask-w8/internals/task2"
+	"github.com/Darari17/weeklytask-w8/internals/task3"
+	"github.com/Darari17/weeklytask-w8/internals/task4"
+)
+
+func main() {
+	task1.DisplayProcessNumber()
+	fmt.Println("")
+	task2.DisplayWebFetcher()
+	fmt.Println("")
+	task3.DisplayUserManagement()
+	fmt.Println("")
+	task4.DisplayGeometris()
+	fmt.Println("")
 }
