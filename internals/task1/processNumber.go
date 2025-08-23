c7cd65ad9b8450ff24ebaf3f9067cca2d07180ff
farid <faridramadhan59@gmail.com>
2025-08-23 14:24:36 +0700

    feat[task1]: add process numbers

---
+++ internals/task1/processNumber.go
@@ -0,0 +1,58 @@
+package task1
+
+import (
+	"errors"
+	"fmt"
+)
+
+func processNumber(numbers []int) ([]int, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("panic:", r)
+		}
+	}()
+
+	if numbers == nil {
+		return nil, errors.New("no data provided")
+	}
+
+	if len(numbers) <= 0 {
+		panic("empty lists provided")
+	}
+
+	result := make([]int, 0)
+	if len(numbers) > 0 {
+		for num := range numbers {
+			result = append(result, numbers[num]*2)
+		}
+	}
+
+	return result, nil
+}
+
+func DisplayProcessNumber() {
+	// valid
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	resA, err := processNumber(a)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resA)
+	}
+
+	// error nil
+	var b []int
+	resB, err := processNumber(b)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resB)
+	}
+
+	// slice kosong panic
+	c := make([]int, 0)
+	_, err = processNumber(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
