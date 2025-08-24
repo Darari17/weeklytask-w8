47522a13fdc61f975d3cb20ef219a6107df51caa
farid <faridramadhan59@gmail.com>
2025-08-24 19:32:11 +0700

    feat: add new user with same ID

--- internals/task3/userManagement.go
+++ internals/task3/userManagement.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func DisplayUserManagement() {
 	userManager := NewUserManager()
-	user1 := &User{ID: 1, Name: "Alice"}
-	user2 := &User{ID: 1, Name: "Bob"}
+	user1 := &User{ID: 1, Name: "Farid"}
+	user2 := &User{ID: 2, Name: "Rhamadhan"}
+
+	user3 := &User{ID: 1, Name: "Darari"} // kalo ID sama
 
 	if err := userManager.AddUser(user1); err != nil {
 		fmt.Println(err)
@@ -15,17 +17,21 @@ func DisplayUserManagement() {
 		fmt.Println(err)
 	}
 
+	if err := userManager.AddUser(user3); err != nil {
+		fmt.Println(err)
+	}
+
 	user, err := userManager.GetUser(1)
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
-		fmt.Printf("User dengan ID %d ditemukan\n", user.ID)
+		fmt.Printf("User dengan ID: %d ditemukan\n", user.ID)
 	}
 
 	user, err = userManager.GetUser(10)
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
-		fmt.Printf("User dengan ID %d ditemukan\n", user.ID)
+		fmt.Printf("User dengan ID: %d ditemukan\n", user.ID)
 	}
 }
